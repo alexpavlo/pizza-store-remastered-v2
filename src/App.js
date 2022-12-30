ebb399449fee1ae6973afbe3805f399105129228
alexpavlo <alexpavlov26.08@gmail.com>
2022-12-30 17:14:31 +0600

    #4

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import Header from './components/Header';
+import Categories from './components/Categories';
+import Sort from './components/Sort';
+import PizzaBlock from './components/PizzaBlock';
+
+import './scss/app.scss';
+
+function App() {
+  return (
+    <div className="wrapper">
+      <Header />
+      <div className="content">
+        <div className="container">
+          <div className="content__top">
+            <Categories />
+            <Sort />
+          </div>
+          <h2 className="content__title">Все пиццы</h2>
+          <div className="content__items">
+            <PizzaBlock title="Мексиканская" price={500} />
+            <PizzaBlock title="hello" price={350} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
